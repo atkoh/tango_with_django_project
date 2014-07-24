3bfbc7b44ffa7993235e1a196fc67a093cfdc93c
atkoh <andy.ts.koh@gmail.com>
2014-07-24 14:17:03 -0400

    first commit

---
+++ djan1.5/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/andy/Self_Learning/Python_Stuff/Django_Practice/code/djan1.5/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
